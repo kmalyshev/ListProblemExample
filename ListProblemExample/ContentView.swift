c902560d26e2404b2b580aa39c7aa63313a0c880
Konstantin Malyshev <ruzambo@gmail.com>
2020-12-27 20:59:36 +0200

    Initial Commit

---
+++ ListProblemExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ListProblemExample
+//
+//  Created by Konstantin Malyshev on 12/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
